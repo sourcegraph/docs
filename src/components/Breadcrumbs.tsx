94cdce43126da6e1579f004c549be1fd7527c6b1
Ahmad Bilal <ahmadbilaldev@gmail.com>
2024-01-31 18:02:07 +0500

    Update breadcrumbs to support catch all routes

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -2,21 +2,17 @@ import Link from 'next/link';
 import clsx from 'clsx';
 import {ChevronRightIcon} from '@heroicons/react/20/solid';
 
-type BreadcrumbsProps = {
-	topic: string;
-	section?: string;
-	subsection?: string;
-	details?: string;
-	subdetails?: string;
-};
+export function Breadcrumbs({path}: {path: string[]}) {
+	const createLink = ({path, index}: {path: string[]; index: number}) => {
+		// For the first index, return the slug itself, otherwise return the joined path
+		return index === 0
+			? `/${encodeURIComponent(path[0])}`
+			: `/${path
+					.slice(0, index + 1)
+					.map(encodeURIComponent)
+					.join('/')}`;
+	};
 
-export function Breadcrumbs({
-	topic,
-	section,
-	subsection,
-	details,
-	subdetails
-}: BreadcrumbsProps) {
 	return (
 		<nav className="mb-8 flex" aria-label="Breadcrumb">
 			<ol role="list" className="flex items-center space-x-4">
@@ -30,109 +26,27 @@ export function Breadcrumbs({
 						</Link>
 					</div>
 				</li>
-
-				<li key={topic}>
-					<div className="flex items-center">
-						<ChevronRightIcon
-							className="h-5 w-5 flex-shrink-0 text-gray-400"
-							aria-hidden="true"
-						/>
-						<Link
-							href={`/${topic}`}
-							className={clsx(
-								!section
-									? 'text-link-light hover:underline dark:text-link'
-									: 'hover:text-link dark:hover:text-link',
-								'ml-4 text-sm font-medium text-gray-500'
-							)}
-						>
-							{topic}
-						</Link>
-					</div>
-				</li>
-
-				{section && (
-					<li key={section}>
+				{path.map((slug, index) => (
+					<li key={slug}>
 						<div className="flex items-center">
 							<ChevronRightIcon
 								className="h-5 w-5 flex-shrink-0 text-gray-400"
 								aria-hidden="true"
 							/>
 							<Link
-								href={`/${topic}/${section}`}
+								href={createLink({path, index})}
 								className={clsx(
-									!subsection
+									'ml-4 text-sm font-medium',
+									index === path.length - 1
 										? 'text-link-light hover:underline dark:text-link'
-										: 'hover:text-link-light dark:hover:text-link',
-									'ml-4 text-sm font-medium text-gray-500'
+										: 'text-gray-500 hover:text-link-light dark:hover:text-link'
 								)}
 							>
-								{section}
+								{slug}
 							</Link>
 						</div>
 					</li>
-				)}
-
-				{subsection && (
-					<li key={subsection}>
-						<div className="flex items-center">
-							<ChevronRightIcon
-								className="h-5 w-5 flex-shrink-0 text-gray-400"
-								aria-hidden="true"
-							/>
-							<Link
-								href={`/${topic}/${section}/${subsection}`}
-								className={clsx(
-									!details
-										? 'text-link-light hover:underline dark:text-link'
-										: 'hover:text-link-light dark:hover:text-link',
-									'ml-4 text-sm font-medium text-gray-500'
-								)}
-							>
-								{subsection}
-							</Link>
-						</div>
-					</li>
-				)}
-
-				{details && (
-					<li key={details}>
-						<div className="flex items-center">
-							<ChevronRightIcon
-								className="h-5 w-5 flex-shrink-0 text-gray-400"
-								aria-hidden="true"
-							/>
-							<Link
-								href={`/${topic}/${section}/${subsection}/${details}`}
-								className={clsx(
-									!subdetails
-										? 'text-link-light hover:underline dark:text-link'
-										: 'hover:text-link-light dark:hover:text-link',
-									'ml-4 text-sm font-medium text-gray-500'
-								)}
-							>
-								{details}
-							</Link>
-						</div>
-					</li>
-				)}
-
-				{subdetails && (
-					<li key={details}>
-						<div className="flex items-center">
-							<ChevronRightIcon
-								className="h-5 w-5 flex-shrink-0 text-gray-400"
-								aria-hidden="true"
-							/>
-							<Link
-								href={`/${topic}/${section}/${subsection}/${details}/${subdetails}`}
-								className="ml-4 text-sm font-medium text-link-light hover:underline dark:text-link"
-							>
-								{subdetails}
-							</Link>
-						</div>
-					</li>
-				)}
+				))}
 			</ol>
 		</nav>
 	);
