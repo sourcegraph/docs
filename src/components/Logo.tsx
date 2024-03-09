ea9eb19fb0799b74cc8378c988820bf3da81ed8d
Maedah Batool <me@MaedahBatool.com>
2024-03-09 09:53:15 -0800

    Fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,13 +3,12 @@ export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
 		<>
 			<img
 				className="hidden h-[23px] w-[190px] dark:block"
-				src="/logo-theme-dark.svg"
+				src="/docs/logo-theme-dark.svg"
 				alt="Sourcegraph Docs"
 			/>
 			<img
 				className="block h-[23px] w-[190px] dark:hidden"
-				// src="/docs/logo-theme-light.svg"
-				src="/logo-theme-light.svg"
+				src="/docs/logo-theme-light.svg"
 				alt="Sourcegraph Docs"
 			/>
 		</>
