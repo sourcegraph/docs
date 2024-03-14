f9c1ada6a8cb31ee13dc9eeadf63a239b5944a75
Maedah Batool <me@MaedahBatool.com>
2024-03-14 10:13:05 -0700

    Add GA Google tag manager script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import {type Metadata} from 'next';
-import {Inter} from 'next/font/google';
-import localFont from 'next/font/local';
+import { Providers } from '@/app/providers';
+import { Layout } from '@/components/Layout';
 import clsx from 'clsx';
-import {Providers} from '@/app/providers';
-import {Layout} from '@/components/Layout';
 import config from 'docs.config';
+import { type Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 
 import '@/styles/tailwind.css';
@@ -35,11 +35,11 @@ export const metadata: Metadata = {
 	}
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html
 			lang="en"
-			style={{colorScheme: 'dark'}}
+			style={{ colorScheme: 'dark' }}
 			className={clsx(
 				'h-full antialiased',
 				inter.variable,
@@ -47,10 +47,31 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 			)}
 			suppressHydrationWarning
 		>
+			<head>
+				{/* Add Google Tag Manager script to head */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-TB4NLS7');`
+					}}
+				/>
+			</head>
+
 			<body
 				suppressHydrationWarning
 				className="flex min-h-full bg-light-bg dark:bg-dark-bg"
 			>
+				{/* Add Google Tag Manager noscript iframe */}
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TB4NLS7"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+					}}
+				/>
+
 				<Providers>
 					<Layout>{children}</Layout>
 				</Providers>
