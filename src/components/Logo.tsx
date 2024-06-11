c9cec9d6b631f21cc16c96e053ce94d05562d1c9
zahidalidev <m.zahidalidev@gmail.com>
2024-06-11 23:46:05 +0500

    Fixed logo path issue

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,24 @@
+import {usePathname} from 'next/navigation'
+
 export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
+	const pathname = usePathname()
+	const docs = extractDocsPath(pathname)
+
+	function extractDocsPath(str: string) {
+    const match = str.match(/^\/docs(?=\/|$)/);
+    return match ? match[0] : '';
+	}
+
 	return (
 		<>
 			<img
 				className="hidden h-[23px] w-[190px] dark:block"
-				src="/docs/logo-theme-dark.svg"
+				src={`${docs}/logo-theme-dark.svg`}
 				alt="Sourcegraph Docs"
 			/>
 			<img
 				className="block h-[23px] w-[190px] dark:hidden"
-				src="/docs/logo-theme-light.svg"
+				src={`${docs}/logo-theme-light.svg`}
 				alt="Sourcegraph Docs"
 			/>
 		</>
