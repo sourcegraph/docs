841c587787f97de98c13b4a3565aebb71bebdb97
zahidalidev <m.zahidalidev@gmail.com>
2024-06-22 02:39:47 +0500

    Updated the icons for edit and questions buttons

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -3,9 +3,10 @@
 import {useCallback, useEffect, useState} from 'react';
 import Link from 'next/link';
 import clsx from 'clsx';
-import {PencilIcon, BugAntIcon} from '@heroicons/react/24/outline'
 import {useParams} from 'next/navigation'
 import {allPosts} from 'contentlayer/generated'
+import { PencilIcon } from './icons/PencilIcon';
+import { BugIcon } from './icons/BugIcon';
 
 interface Heading {
 	level: number;
@@ -162,7 +163,7 @@ export function TableOfContents({headings}: Props) {
 								rel="noopener noreferrer"
 								className="flex items-center text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
 							>
-								<BugAntIcon className="mr-1 h-4 w-4" />
+								<BugIcon className="mr-1 h-4 w-4" />
 								Questions? Give us feedback
 							</a>
 						</div>
