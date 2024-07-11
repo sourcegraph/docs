c6c397cda9ce012c6942e1243d94ed1c67048e8d
Ahmad Bilal <ahmadbilaldev@gmail.com>
2024-07-11 20:07:22 +0500

    👌 IMPROVE: Icons

---
+++ src/components/icons/UserIcon.tsx
@@ -0,0 +1,19 @@
+export function IconUser({className}: {className?: string}) {
+	return (
+		<svg
+			className={className}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+		>
+			<path
+				d="M4 20V19C4 16.7909 5.79086 15 8 15H16C18.2091 15 20 16.7909 20 19V20M16.5 7.5C16.5 9.98528 14.4853 12 12 12C9.51472 12 7.5 9.98528 7.5 7.5C7.5 5.01472 9.51472 3 12 3C14.4853 3 16.5 5.01472 16.5 7.5Z"
+				stroke="currentColor"
+				strokeWidth="2"
+				strokeLinecap="round"
+			/>
+		</svg>
+	);
+}
