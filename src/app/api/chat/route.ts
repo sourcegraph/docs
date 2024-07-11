1af3cb21c533b3ceb7a8070b65105690442279c3
Ahmad Bilal <ahmadbilaldev@gmail.com>
2024-07-11 20:08:03 +0500

    🐛 FIX: Route and streaming

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,6 @@
-export const runtime = 'edge'
+import {OpenAIStream, StreamingTextResponse} from 'ai';
+
+export const runtime = 'edge';
 
 /**
  * Stream AI Chat Messages from Langbase
@@ -7,46 +9,52 @@ export const runtime = 'edge'
  * @returns
  */
 export async function POST(req: Request) {
-  try {
-    console.log('NEXT_LB_PIPE_API_KEY: ', process.env?.NEXT_LB_PIPE_API_KEY)
-    if (!process.env.NEXT_LB_PIPE_API_KEY) {
-      throw new Error(
-        'Please set NEXT_LB_PIPE_API_KEY in your environment variables.'
-      )
-    }
+	try {
+		console.log(
+			'NEXT_LB_PIPE_API_KEY: ',
+			process.env?.NEXT_LB_PIPE_API_KEY
+		);
+		if (!process.env.NEXT_LB_PIPE_API_KEY) {
+			throw new Error(
+				'Please set NEXT_LB_PIPE_API_KEY in your environment variables.'
+			);
+		}
 
-    const endpointUrl = 'https://api.langbase.com/beta/chat'
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.NEXT_LB_PIPE_API_KEY}`
-    }
+		const endpointUrl = 'https://api.langbase.com/beta/chat';
+		const headers = {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${process.env.NEXT_LB_PIPE_API_KEY}`
+		};
 
-    // Get chat prompt messages and threadId from the client.
-    const body = await req.json()
-    const { messages, threadId } = body
+		// Get chat prompt messages and threadId from the client.
+		const body = await req.json();
+		const {messages, threadId} = body;
 
-    const requestBody = {
-      messages,
-      ...(threadId && { threadId }) // Only include threadId if it exists
-    }
+		const requestBody = {
+			messages,
+			...(threadId && {threadId}) // Only include threadId if it exists
+		};
 
-    // Send the request to Langbase API.
-    const response = await fetch(endpointUrl, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(requestBody)
-    })
+		// Send the request to Langbase API.
+		const response = await fetch(endpointUrl, {
+			method: 'POST',
+			headers,
+			body: JSON.stringify(requestBody)
+		});
 
-    if (!response.ok) {
-      const res = await response.json()
-      throw new Error(`Error ${res.error.status}: ${res.error.message}`)
-    }
+		if (!response.ok) {
+			const res = await response.json();
+			throw new Error(`Error ${res.error.status}: ${res.error.message}`);
+		}
 
-    return new Response(response.body as ReadableStream<Uint8Array>, {
-      headers: response.headers
-    })
-  } catch (error: any) {
-    console.error('Uncaught API Error:', error)
-    return new Response(JSON.stringify(error), { status: 500 })
-  }
+		// Handle Langbase response, which is a stream in OpenAI format.
+		const stream = OpenAIStream(response);
+		// Respond with a text stream.
+		return new StreamingTextResponse(stream, {
+			headers: response.headers
+		});
+	} catch (error: any) {
+		console.error('Uncaught API Error:', error);
+		return new Response(JSON.stringify(error), {status: 500});
+	}
 }
