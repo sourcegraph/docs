b64af2a66a72aababeb4effec34e201ea8370559
Maedah Batool <me@MaedahBatool.com>
2024-08-07 13:55:58 -0700

    Docs for Prompts and Prompt Library (#546)
    Adds docs for the upcoming Prompts and the Prompt Library.
    
    -
    https://linear.app/sourcegraph/issue/PROD-125/create-docs-for-prompts-and-prompt-library

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -47,8 +47,8 @@ export const navigation: NavigationItem[] = [
             subsections: [
               { title: "Chat", href: "/cody/capabilities/chat", },
               { title: "Autocomplete", href: "/cody/capabilities/autocomplete", },
-              { title: "Commands", href: "/cody/capabilities/commands", },
-              { title: "OpenCtx" , href: "/cody/capabilities/openctx", },
+              { title: "Prompts & Commands", href: "/cody/capabilities/commands", },
+              { title: "OpenCtx", href: "/cody/capabilities/openctx", },
               { title: "Debug Code", href: "/cody/capabilities/debug-code", },
               { title: "Context Filters", href: "/cody/capabilities/ignore-context", },
               { title: "Proxy Setup", href: "/cody/capabilities/proxy-setup", },
