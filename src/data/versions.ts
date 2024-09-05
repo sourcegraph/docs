4b7c4971cf31a3b68ae2424996e513e21de78297
Maedah Batool <me@MaedahBatool.com>
2024-09-04 18:07:01 -0700

    Docs working branch for the September 4th release (#603)
    This is the working branch for Sourcegraph v5.7 scheduled for release on
    Sep 4th.
    
    - Search Jobs GA
    - Cody for Web GA
    - JetBrains unified chat panel
    - Smart Apply in JetBrains
    - DeepSeek-V2 and Claude 3.5 default models
    
    ---------
    
    Co-authored-by: Stefan Hengl <stefan.hengl@gmail.com>
    Co-authored-by: Michael Bahr <1830132+bahrmichael@users.noreply.github.com>
    Co-authored-by: Camden Cheek <camden@ccheek.com>

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -15,8 +15,8 @@ export const versions: VersionI[] = [
         url: '/',
     },
     {
-        name: 'v5.5',
-        url: 'https://5.5.sourcegraph.com/'
+        name: 'v5.6',
+        url: 'https://5.6.sourcegraph.com/'
     },
 ];
 
