12b29f120fda1fe33926bad25414a7372ce5d5f4
Alex Kemper <alex.j.kemper@gmail.com>
2024-09-12 12:44:37 -0400

    Adds a 'How to videos' section to side nav and the content for those … (#637)
    Adds TS Loom videos per TS request
    
    ---------
    
    Co-authored-by: Alex Kemper <alexkemper@alexs-mbp.lan>

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -307,6 +307,14 @@ export const navigation: NavigationItem[] = [
         href: "/sla",
       },
       {
+        title: "How to videos",
+        href: "/how-to-videos",
+        sections: [
+          { title: "Code Search", href: "/how-to-videos/code-search" },
+          { title: "Cody", href: "/how-to-videos/cody" },
+        ],
+      },
+      {
         title: "Tutorials",
         href: "/tutorials",
       },
