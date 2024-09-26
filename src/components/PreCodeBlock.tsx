d21bbf14a0c2d1c013753d5061ccbded24f7dbcc
Anish Lakhwara <anish+git@lakhwara.com>
2024-09-26 13:53:59 -0700

    POC for Versions being updated from the ReleaseRegistry
    Any Cody block which has {CURRENT_VERSION} will be replaced by the
    latest release from the ReleaseRegistry.
    This PR also fixes the PreCodeBlock component, which previously didn't
    really work at all. The copy function was broken, and the children check
    didn't do anything (child is never of type string in react)
    
    Up next is cleaning up the Versions file (it'll be removed)
    And then replacing all hard coded versions with the same process is the
    POC. Then we'll also need some automation which deploys / builds the
    website at release time since the release version gets determined at
    compile time rather than client time.

--- src/components/PreCodeBlock.tsx
+++ src/components/PreCodeBlock.tsx
@@ -1,26 +1,53 @@
 import { CopyButton } from '@/components/CopyButton'
-import React, { DetailedHTMLProps, HTMLAttributes, Children } from 'react'
+import React, { DetailedHTMLProps, HTMLAttributes, Children, useEffect, useState } from 'react'
 
 interface PreProps extends DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement> {
   raw?: string
 }
 
-export function PreCodeBlock ({ children, ...props }: PreProps) {
+async function getLatestVersion() {
+  const url = new URL('https://releaseregistry.sourcegraph.com/v1/releases/sourcegraph/latest');
+
+  const response = await fetch(url.toString(), {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return await response.json();
+}
+
+export async function PreCodeBlock({ children, ...props }: PreProps) {
   const propsObj = { ...props }
   const propsValues = Object.values(propsObj)
   const [, , , dataLanguage, dataTheme] = propsValues
   const lang = dataLanguage || "shell"
 
-  let codeContent = ''
-  Children.forEach(children, child => {
-    if (typeof child === 'string') codeContent += child
-  })
+  let codeContent = props?.raw || ''
+
+  // If raw prop is not available, try to extract content from children
+  if (!codeContent && children) {
+    React.Children.forEach(children, (child) => {
+      if (React.isValidElement(child) && child.props.children) {
+        codeContent += child.props.children
+      }
+    })
+  }
+
+  const version = await getLatestVersion()
+  const latestVersion = version.version
+  codeContent = codeContent.replace(/{CURRENT_VERSION}/g, latestVersion)
 
   return (
     <div className="relative">
       <pre className="pt-12" data-language={lang} data-theme={dataTheme}>
-        <CopyButton lang={lang} text={props?.raw || ''}/>
-        {children}
+        <CopyButton lang={lang} text={codeContent} />
+        {codeContent}
       </pre>
     </div>
   )
