32861982175f1bb5ae5e729a0ef1e6fafa51e3f2
Anish Lakhwara <anish+git@lakhwara.com>
2024-09-26 15:55:18 -0700

    also provide a function that can return the version, so we can use it outside of code blocks

--- src/components/PreCodeBlock.tsx
+++ src/components/PreCodeBlock.tsx
@@ -1,8 +1,21 @@
 import { CopyButton } from '@/components/CopyButton'
-import React, { DetailedHTMLProps, HTMLAttributes, Children, useEffect, useState } from 'react'
+import React, { DetailedHTMLProps, HTMLAttributes, Children } from 'react'
 
 interface PreProps extends DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement> {
   raw?: string
+}
+
+interface ReleaseInfo {
+  id: number;
+  name: string;
+  public: boolean;
+  created_at: string;
+  promoted_at: string;
+  version: string;
+  git_sha: string;
+  is_development: boolean;
+  tags: string[] | null;
+  canonical_name: string;
 }
 
 async function getLatestVersion() {
@@ -19,11 +32,14 @@ async function getLatestVersion() {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
 
-  return await response.json();
+  const releaseInfo: ReleaseInfo = await response.json();
+  return releaseInfo.version
 }
 
-const CURRENT_VERSION_S = getLatestVersion()
-export const CURRENT_VERSION_STRING = CURRENT_VERSION_S.version
+export async function CURRENT_VERSION_STRING() {
+  const v = await getLatestVersion()
+  return v
+}
 
 export async function PreCodeBlock({ children, ...props }: PreProps) {
   const propsObj = { ...props }
@@ -43,8 +59,7 @@ export async function PreCodeBlock({ children, ...props }: PreProps) {
   }
 
   const version = await getLatestVersion()
-  const latestVersion = version.version
-  codeContent = codeContent.replace(/{CURRENT_VERSION}/g, latestVersion)
+  codeContent = codeContent.replace(/{CURRENT_VERSION}/g, version)
 
   return (
     <div className="relative">
