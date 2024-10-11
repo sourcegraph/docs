c298a786223c11ed3930dfdc65419d97558f998a
Ahmad Bilal <ahmadbilaldev@gmail.com>
2024-10-11 22:30:41 +0500

    📦 NEW: Deployed docs in memory

--- baseai/memory/docs/index.ts
+++ baseai/memory/docs/index.ts
@@ -5,6 +5,7 @@ const memoryDocs = (): MemoryI => ({
   name: 'docs',
   description: 'Docs folder of sourcegraph docs repository as an auto-synced memory',
   config: {
+    deployedCommitHash: '7a147cfb18f19d5584ffb3805b678db47191960a',
 		useGitRepo: true,
 		dirToTrack: path.posix.join('.', 'docs'),
 		extToTrack: [".md",".mdx"]
