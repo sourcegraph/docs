7a147cfb18f19d5584ffb3805b678db47191960a
Ahmad Bilal <ahmadbilaldev@gmail.com>
2024-10-11 21:49:13 +0500

    📦 NEW: Setup memory using BaseAI

---
+++ baseai/memory/docs/index.ts
@@ -0,0 +1,14 @@
+import { MemoryI } from '@baseai/core';
+import path from 'path';
+
+const memoryDocs = (): MemoryI => ({
+  name: 'docs',
+  description: 'Docs folder of sourcegraph docs repository as an auto-synced memory',
+  config: {
+		useGitRepo: true,
+		dirToTrack: path.posix.join('.', 'docs'),
+		extToTrack: [".md",".mdx"]
+  }
+});
+
+export default memoryDocs;
