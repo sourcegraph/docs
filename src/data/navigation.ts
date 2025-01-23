a991ec12a74a797bb02ea87c3728f622d714de15
Maedah Batool <me@MaedahBatool.com>
2025-01-23 20:00:55 +0500

    Nav structure

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -303,6 +303,27 @@ export const navigation: NavigationItem[] = [
 		],
 	},
 	{
+		separator: "Pricing",
+		topics: [
+			{
+				title: "Free",
+				href: "/pricing/free",
+			},
+			{
+				title: "Enterprise Starter",
+				href: "/pricing/enterprise-starter",
+				sections: [
+					{ title: "Billing", href: "/pricing/billing" },
+
+				],
+			},
+			{
+				title: "Enterprise",
+				href: "/pricing/enterprise",
+			},
+		],
+	},
+	{
 		separator: "Help & Support",
 		topics: [
 			{
