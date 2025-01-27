26a7ba823e3f8820dc7be4293be6fe9558312943
Maedah Batool <me@MaedahBatool.com>
2025-01-27 10:18:23 -0800

    Update src/data/navigation.ts
    Co-authored-by: Aravind Ramaraju <51247042+aramaraju@users.noreply.github.com>

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -56,7 +56,7 @@ export const navigation: NavigationItem[] = [
 							{ title: "Chat", href: "/cody/capabilities/chat", },
 							{ title: "Autocomplete", href: "/cody/capabilities/autocomplete", },
               { title: "Prompts", href: "/cody/capabilities/prompts", },
-							{ title: "Agentic Chat", href: "/cody/capabilities/agentic-chat", },
+							{ title: "Agentic chat", href: "/cody/capabilities/agentic-chat", },
 							{ title: "OpenCtx", href: "/cody/capabilities/openctx", },
 							{ title: "Debug Code", href: "/cody/capabilities/debug-code", },
 							{ title: "Context Filters", href: "/cody/capabilities/ignore-context", },
