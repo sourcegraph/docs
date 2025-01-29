8f5945716f559490bbfb9db37d6d90ff3da6b978
Maedah Batool <me@MaedahBatool.com>
2025-01-29 07:34:26 -0800

    Update OG image for docs with new brand guidelines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import MdxComponents from '@/components/MdxComponents';
-import { Prose } from '@/components/Prose';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import { allPosts } from 'contentlayer/generated';
-import { getMDXComponent } from 'next-contentlayer/hooks';
-import { notFound } from 'next/navigation';
+import {Prose} from '@/components/Prose';
+import {GoogleAnalytics} from '@next/third-parties/google';
+import {allPosts} from 'contentlayer/generated';
+import {getMDXComponent} from 'next-contentlayer/hooks';
+import {notFound} from 'next/navigation';
 
 export const generateMetadata = () => {
 	return {
 		openGraph: {
-			images: [{url: 'https://sourcegraph.com/docs/sourcegraph-docs-c.png'}]
+			images: [
+				{url: 'https://sourcegraph.com/docs/sourcegraph-og-nw.png'}
+			]
 		}
-	}
-}
+	};
+};
 
 const PostLayout = () => {
 	const post = allPosts.find(post => post._raw.flattenedPath === '');
@@ -29,7 +31,6 @@ const PostLayout = () => {
 			</div>
 			<GoogleAnalytics gaId="GTM-TB4NLS7" />
 		</>
-
 	);
 };
 
