1082cb9e041aae1927157e35260de6a3977ad543
Maedah Batool <me@MaedahBatool.com>
2025-01-29 20:38:05 +0500

    Update OG image for docs with new brand guidelines (#936)
    Update the OG image with the new brand guidelines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
-import { Providers } from '@/app/providers';
-import { Layout } from '@/components/Layout';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import {Providers} from '@/app/providers';
+import {Layout} from '@/components/Layout';
+import {GoogleAnalytics} from '@next/third-parties/google';
 import clsx from 'clsx';
 import config from 'docs.config';
-import { type Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import {type Metadata} from 'next';
+import {Inter} from 'next/font/google';
 import localFont from 'next/font/local';
-import Chatbot from '@/components/ChatBot';
 
-import '@langbase/components/styles'
 import '@/styles/tailwind.css';
+import '@langbase/components/styles';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -30,60 +29,60 @@ export const metadata: Metadata = {
 		template: '%s - Sourcegraph docs',
 		default: 'Sourcegraph docs'
 	},
-	description: 'Documentation for Sourcegraph, the code intelligence platform.',
+	description:
+		'Documentation for Sourcegraph, the code intelligence platform.',
 	other: {
-		"docsearch:language": "en",
-		"docsearch:version": `v${config.DOCS_LATEST_VERSION}`
+		'docsearch:language': 'en',
+		'docsearch:version': `v${config.DOCS_LATEST_VERSION}`
 	},
 	alternates: {
 		canonical: '/docs'
 	},
 	openGraph: {
-		images: [{ url: 'https://sourcegraph.com/docs/sourcegraph-docs-c.png' }]
+		images: [{url: 'https://sourcegraph.com/docs/sourcegraph-og-nw.png'}]
 	}
 };
 
-
 // PolySans Variable Font
 const polySansVariable = localFont({
-  src: [
-    {
-      path: '../fonts/PolySans-Neutral.woff',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/PolySans-NeutralItalic.woff',
-      style: 'italic',
-    }
-  ],
-  variable: '--font-polysans'
+	src: [
+		{
+			path: '../fonts/PolySans-Neutral.woff',
+			style: 'normal'
+		},
+		{
+			path: '../fonts/PolySans-NeutralItalic.woff',
+			style: 'italic'
+		}
+	],
+	variable: '--font-polysans'
 });
 
 // PolySans Mono Font
 const polySansMono = localFont({
-  src: [
-    {
-      path: '../fonts/PolySans-NeutralMono.woff',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/PolySans-SlimMono.woff',
-      style: 'normal',
-    }
-  ],
-  variable: '--font-polysans-mono'
+	src: [
+		{
+			path: '../fonts/PolySans-NeutralMono.woff',
+			style: 'normal'
+		},
+		{
+			path: '../fonts/PolySans-SlimMono.woff',
+			style: 'normal'
+		}
+	],
+	variable: '--font-polysans-mono'
 });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<html
 			lang="en"
-			style={{ colorScheme: 'dark' }}
+			style={{colorScheme: 'dark'}}
 			className={clsx(
-        'h-full antialiased',
-        polySansVariable.variable,
-        polySansMono.variable
-      )}
+				'h-full antialiased',
+				polySansVariable.variable,
+				polySansMono.variable
+			)}
 			suppressHydrationWarning
 		>
 			<head>
