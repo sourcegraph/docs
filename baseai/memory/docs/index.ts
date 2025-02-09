39f2778ad2dce348bb762a85f765c21453cec4fe
Maedah Batool <me@MaedahBatool.com>
2025-02-10 04:58:28 +0500

    🐛 FIX: memory config (#966)
    <!-- Explain the changes introduced in your PR -->
    
    ## Pull Request approval
    
    You will need to get your PR approved by at least one member of the
    Sourcegraph team. For reviews of docs formatting, styles, and component
    usage, please tag the docs team via the #docs Slack channel.

--- baseai/memory/docs/index.ts
+++ baseai/memory/docs/index.ts
@@ -1,16 +1,16 @@
 import { MemoryI } from '@baseai/core';
 
 const memoryDocs = (): MemoryI => ({
-  name: 'docs',
-  description: 'Docs folder of sourcegraph docs repository as an auto-synced memory',
+	name: 'docs',
+	description: 'Docs folder of sourcegraph docs repository as an auto-synced memory',
 	git: {
 		enabled: true,
-		include: ['**/*'],
+		include: ['**/*.mdx'],
 		gitignore: true,
 		deployedAt: '',
 		embeddedAt: '',
 	},
-  documents: {
+	documents: {
 		meta: doc => {
 			const url = `https://sourcegraph.com/docs/${doc.path}`;
 			return {
