95f91cdf01c37ddaf870980dc1a4f389c171829d
Maedah Batool <me@MaedahBatool.com>
2025-03-18 18:13:08 -0700

    🐛 FIX: no need to rewriter

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,35 +9,11 @@ const langbase = new Langbase({
 
 export async function POST(req: NextRequest) {
 	const options = await req.json();
-	console.log("🚀 ~ options:", options)
-
-	const { completion } = await langbase.pipes.run({
-		name: 'query-rewrite',
-		messages: [
-			{
-				role: 'user',
-				content: options.messages[options.messages.length - 1].content,
-			},
-		],
-		stream: false
-	});
-	console.log("🚀 ~ completion:", completion)
-
-
-	// Parse the completion to get the rewritten query
-	const rewrittenQuery = JSON.parse(completion).rewrittenQuery;
-	console.log("🚀 ~ rewrittenQuery:", rewrittenQuery)
+	// console.log("🚀 ~ options:", options)
 
 	// Create a new options object with the rewritten query
 	const updatedOptions = {
 		...options,
-		messages: [
-			...options.messages.slice(0, -1), // Keep all messages except the last one
-			{
-				role: 'user',
-				content: rewrittenQuery
-			}
-		],
 		name: 'ask-sourcegraph-docs',
 		lastMessageOnly: true,
 	};
