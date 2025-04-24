38bd4b0777eb161ef71e91a2cd298840f1964637
Maedah Batool <me@MaedahBatool.com>
2025-04-24 11:15:11 -0700

    Fix legacy versions (#1097)
    - https://sourcegraph.slack.com/archives/C01DXLN3D0T/p1745503697479829

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -12,7 +12,7 @@ export const versions: VersionI[] = [
     {
         name: `v${config.DOCS_LATEST_VERSION}`,
         label: 'latest',
-        url: '/',
+        url: '/docs',
     },
     {
         name: 'v6.1',
